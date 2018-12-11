a092892f1c9f641cc1b4228cfc93dc034245ca74
Eddie Fong <fonged@oocl.com>
2018-12-11 18:04:29 +0800

    [Eddie] fix: fix auto refresh issue

--- src/component/ParkingLotDashboard.js
+++ src/component/ParkingLotDashboard.js
@@ -1,47 +1,48 @@
 import React, { Component } from 'react'
-import { Card, Col, Row,Progress } from 'antd';
+import { Card, Col, Row, Progress } from 'antd';
 import DashboardResource from '../resources/DashboardResource';
 export default class ParkingLotDashboard extends Component {
-state= {results : []}
-    getAllParkingLot(){
-        fetch('https://parking-system-backend.herokuapp.com/parkinglots')
-        .then(result =>{
-            return result.json();
-        })
-        .then(result =>{
-            this.setState({results: result})            
-        })
+    constructor(props) {
+        super(props);
+        this.state = { results: [] }
     }
 
-  render(){
-    this.getAllParkingLot()
-    let items = this.state.results;
-    // function handleChange(value) {
-    //     console.log(`selected ${value}`);
-    // }
-    return(
+    componentDidMount() {
+        fetch('https://parking-system-backend.herokuapp.com/parkinglots')
+        .then(result => {
+            return result.json();
+        })
+        .then(result => {
+            this.setState({ results: result })
+        })      
+      }
+
+    render() {
         
-        <div>
-            <Row gutter={16}>               
-            {items.map(item =>
-                        <Col span={8} style={{marginBottom:20}}>
+        let items = this.state.results;
+        return (
+
+            <div>
+                <Row gutter={16}>
+                    {items.map(item =>
+                        <Col span={8} style={{ marginBottom: 20 }}>
                             <Card title={item.parkingLotName}>
                                 <Row>
                                     <Col span={12}>
                                         <Progress
                                             type="dashboard"
-                                            format={() => `${item.capacity-item.availablePositionCount}/${item.capacity}`}
-                                            percent={((item.capacity-item.availablePositionCount) / item.capacity) * 100}/>
+                                            format={() => `${item.capacity - item.availablePositionCount}/${item.capacity}`}
+                                            percent={((item.capacity - item.availablePositionCount) / item.capacity) * 100} />
                                     </Col>
                                     <Col span={12}>
                                         ParkingBoy：{item.employee_id}
                                     </Col>
                                 </Row>
-                                <Col span={12}><b><br/>Status</b></Col>
+                                <Col span={12}><b><br />Status</b></Col>
                             </Card>
                         </Col>)}
-            </Row>
-        </div>
-    );
-}
+                </Row>
+            </div>
+        );
+    }
 }
