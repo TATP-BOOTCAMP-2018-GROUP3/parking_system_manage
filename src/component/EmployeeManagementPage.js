abf5d7670916000d3a822e1a324dce1d587265f6
Skittle Wan <skittle.wan@oocl.com>
2018-12-11 09:59:59 +0800

    [Skittle] feat: implement basic ui structure

---
+++ src/component/EmployeeManagementPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Table, Divider, Tag } from 'antd';
+
+export default class EmployeeManagementPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Employees</h1>
+
+      </div>
+    )
+  }
+}
---
+++ src/component/ParkingLotDashboard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ParkingLotDashboard extends Component {
+  render() {
+    return (
+      <div>
+        <h1>ParkingLotDashboard</h1>
+      </div>
+    )
+  }
+}
