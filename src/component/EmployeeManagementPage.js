82fcf7ccb908a62c0c0d7e60077cdca175efa6fc
Eddie Fong <fonged@oocl.com>
2018-12-12 17:50:00 +0800

    [Eddie] feat: init employee page

--- src/component/EmployeeManagementPage.js
+++ src/component/EmployeeManagementPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { Table, Divider, Button, Input} from 'antd';
+import { Table, Divider, Button, Input, Popconfirm } from 'antd';
+import ParkingClerksResource from '../resources/ParkingClerksResource';
 
 const Search = Input.Search;
 
@@ -10,16 +11,20 @@ const columns = [{
   render: text => <a href="javascript:;">{text}</a>,
 }, {
   title: 'Name',
-  dataIndex: 'name',
-  key: 'name',
-}, {
-  title: 'Email',
-  dataIndex: 'email',
-  key: 'email',
+  dataIndex: 'accountName',
+  key: 'accountName',
 }, {
   title: 'Phone Number',
-  dataIndex: 'phoneNumb',
-  key: 'phoneNumb',
+  dataIndex: 'phoneNum',
+  key: 'phoneNum',
+}, {
+  title: 'Role',
+  dataIndex: 'role',
+  key: 'role',
+}, {
+  title: 'Working Status',
+  dataIndex: 'workingStatus',
+  key: 'workingStatus',
 }, {
   title: 'Action',
   key: 'action',
@@ -27,54 +32,64 @@ const columns = [{
     <span>
       <a href="javascript:;">Edit</a>
       <Divider type="vertical" />
-      <a href="javascript:;">Delete</a>
+      <Popconfirm title="Sure to delete?" onConfirm={() => {
+        const data = this.state.data
+        this.setState({
+          data: data.filter(item => item.key !== record.key)
+        })
+      }}>
+        <a href="javascript:;">Freeze</a>
+      </Popconfirm>
     </span>
   ),
 }];
 
-const data = [{
-  key: '1',
-  id: "1",
-  name: 'John Brown',
-  email: 'testemail1@gamil.com',
-  phoneNumb: "11111111",
- 
-}, {
-  key: '2',
-  id: '2',
-  name: 'Jim Green',
-  email: 'testemail2@gamil.com',
-  phoneNumb: "22222222",
 
-}, {
-  key: '3',
-  id: '3',
-  name: 'Joe Black',
-  email: 'testemail3@gamil.com',
-  phoneNumb: "3333333",
-}];
-
-
- 
 
 export default class EmployeeManagementPage extends Component {
-  render(){ 
-    return ( 
-    <div>
-      <Button onClick={this.handleAdd} type="primary" style={{ marginBottom: 16 }}>
-        Add Employee
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    const hostname = "https://parking-system-backend.herokuapp.com";
+    const resourceName = "/employees";
+    fetch(hostname + resourceName ,
+      {
+          method: 'GET', 
+          mode: 'cors',
+          headers: new Headers({
+              'Authorization': 'Bearer ' + localStorage.getItem('AUTH')
+          })
+      })
+      .then(res => res.json())
+      .then(res => {
+        console.log(res)
+        this.setState({ data: res })
+      });
+  }
+
+  render() {
+
+    return (
+      <div>
+        <Button onClick={this.handleAdd} type="primary" style={{ marginBottom: 16 }}>
+          Add Employee
       </Button>
-      <span>
-        <div>
-          <Search
-            placeholder="input search text"
-            onSearch={value => console.log(value)}
-            style={{ width: 200 }}
-          />
-        </div>
-      </span>
-    <Table columns={columns} dataSource={data} />
-    </div>
+        <span>
+          <div>
+            <Search
+              placeholder="input search text"
+              onSearch={value => console.log(value)}
+              style={{ width: 200 }}
+            />
+          </div>
+        </span>
+        <Table columns={columns} dataSource={this.state.data} />
+      </div>
     )
   }
 }
