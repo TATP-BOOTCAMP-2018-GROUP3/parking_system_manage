d716dc77d06d89c84b9539314047da3dec58621e
Wong Cho Hing <venus.wong@oocl.com>
2018-12-14 01:14:50 +0800

    [venus] feat: add search function by any detail listed in table for order management page

--- src/component/OrderManagement.js
+++ src/component/OrderManagement.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Table, Tag, Select, Input } from 'antd';
+import { Table, Tag, Select, Input, Form } from 'antd';
 import ParkingOrderResource from '../resources/ParkingOrderResource';
 import ReturnOrderResource from '../resources/ReturnOrderResource';
 import ParkingClerksResource from '../resources/ParkingClerksResource';
@@ -7,7 +7,122 @@ import ParkingClerksResource from '../resources/ParkingClerksResource';
 const Search = Input.Search;
 const Option = Select.Option;
 
+const FormItem = Form.Item;
+
+const EditableContext = React.createContext();
+
+const EditableRow = ({ form, index, ...props }) => (
+  <EditableContext.Provider value={form}>
+    <tr {...props} />
+  </EditableContext.Provider>
+);
+
+const EditableFormRow = Form.create()(EditableRow);
+
+class EditableCell extends React.Component {
+  state = {
+    editing: false,
+  }
+
+  componentDidMount() {
+    if (this.props.editable) {
+      document.addEventListener('click', this.handleClickOutside, true);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.props.editable) {
+      document.removeEventListener('click', this.handleClickOutside, true);
+    }
+  }
+
+  toggleEdit = () => {
+    const editing = !this.state.editing;
+    this.setState({ editing }, () => {
+      if (editing) {
+        this.input.focus();
+      }
+    });
+  }
+
+  handleClickOutside = (e) => {
+    const { editing } = this.state;
+    if (editing && this.cell !== e.target && !this.cell.contains(e.target)) {
+      this.save();
+    }
+  }
+
+  save = () => {
+    const { record, handleSave } = this.props;
+    this.form.validateFields((error, values) => {
+      if (error) {
+        return;
+      }
+      this.toggleEdit();
+      handleSave({ ...record, ...values });
+    });
+  }
+  render() {
+    const { editing } = this.state;
+    const {
+      editable,
+      dataIndex,
+      title,
+      record,
+      index,
+      handleSave,
+      ...restProps
+    } = this.props;
+    return (
+      <td ref={node => (this.cell = node)} {...restProps}>
+        {editable ? (
+          <EditableContext.Consumer>
+            {(form) => {
+              this.form = form;
+              return (
+                editing ? (
+                  <FormItem style={{ margin: 0 }}>
+                    {form.getFieldDecorator(dataIndex, {
+                      rules: [{
+                        required: true,
+                        message: `${title} is required.`,
+                      }],
+                      initialValue: record[dataIndex],
+                    })(
+                      <Input
+                        ref={node => (this.input = node)}
+                        onPressEnter={this.save}
+                      />
+                    )}
+                  </FormItem>
+                ) : (
+                    <div
+                      className="editable-cell-value-wrap"
+                      style={{ paddingRight: 24 }}
+                      onClick={this.toggleEdit}
+                    >
+                      {restProps.children}
+                    </div>
+                  )
+              );
+            }}
+          </EditableContext.Consumer>
+        ) : restProps.children}
+      </td>
+    );
+  }
+}
+
 export default class OrderManagementPage extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      orders: this.props.orders,
+      searching: false
+    }
+  }
+
   componentWillMount() {
     if (localStorage.getItem('AUTH') === null || localStorage.getItem('AUTH') === '') {
         this.props.history.push('/login');
@@ -140,19 +255,78 @@ export default class OrderManagementPage extends Component {
     .then(res => this.initailAllData())
   }
 
+  resetSearch = () => {
+    this.setState(
+      {
+        orders: this.props.orders,
+        searching: false
+      }
+    )
+  }
+
+  searchOrder = (value) => {
+    let tempOrder = this.props.orders.map((order) => {
+      return {
+        carId: order.carId,
+        type: order.type,
+        id: order.id,
+        status: order.status,
+        phoneNumber: order.phoneNumber,
+        searchString: order.carId + "|" + order.type + "|" + order.id + "|" + order.status + "|" + order.phoneNumber + "|"
+      }
+    })
+    this.setState({
+      orders: tempOrder.filter((order) => (order.searchString.indexOf(value) !== -1)),
+      searching: true
+    },
+    )
+  }
+
+
   render() {
+
+    const components = {
+      body: {
+        row: EditableFormRow,
+        cell: EditableCell,
+      },
+    };
+
+    const columns = this.createColumns().map((col) => {
+      if (!col.editable) {
+        return col;
+      }
+      return {
+        ...col,
+        onCell: record => ({
+          record,
+          editable: col.editable,
+          dataIndex: col.dataIndex,
+          title: col.title,
+          handleSave: this.handleSave,
+        }),
+      };
+    });
+
     return (
       <div>
         <span>
           <div>
-            <Search
-              placeholder="input search text"
-              onSearch={value => console.log(value)}
-              style={{ width: 200 }}
-            />
+          <Search
+            placeholder="Search order"
+            onSearch={(value) => { this.searchOrder(value) }}
+            style={{ width: 200 }}
+          />
           </div>
         </span>
-        <Table title={() => 'Orders'} columns={this.createColumns()} dataSource={this.props.orders} />
+        <Table 
+          title={() => 'Orders'} 
+          components={components}
+          rowClassName={() => 'editable-row'}
+          bordered
+          dataSource={(this.state.searching) ? this.state.orders : this.props.orders}
+          columns={columns}
+        />
       </div>
     )
   }
